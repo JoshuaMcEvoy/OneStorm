668de72a45770412719906aab92571245cf4ad15
Josh <josh@Joshs-MBP.lan>
2018-03-27 22:05:46 +1100

    map styling

--- js/main.js
+++ js/main.js
@@ -10,7 +10,6 @@ $(document).ready(function(){
   $('#submit-button').on('click', function() {
       let address = $('#address').val();
       let date = $('#date').val();
-      console.log(date);
 
       // first request to geocoder to return list of possible results
       $.getJSON({
@@ -44,13 +43,6 @@ $(document).ready(function(){
       let houseLocation = data.results[0].geometry.location;
       let lat = houseLocation.lat;
       let lng = houseLocation.lng;
-      console.log(`lat = ${lat}`);
-      console.log(`lng = ${lng}`);
-
-      console.log(new google.maps.LatLng(lat, lng))
-      // console.log(google.maps.LatLng(data.results[0].geometry.location.lat, data.results[0].geometry.location.lng));
-      // console.log(data.results[0].geometry.location.lat);
-      // console.log(routePolygon);
       google.maps.geometry.poly.containsLocation(new google.maps.LatLng(lat, lng), routePolygon) ?
       console.log(`you're covered`) :
       console.log(`not in area `);
@@ -76,6 +68,8 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4.3,
+    // style for the map
+    styles: [{"featureType":"all","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"all","elementType":"geometry.fill","stylers":[{"visibility":"on"}]},{"featureType":"all","elementType":"geometry.stroke","stylers":[{"visibility":"on"}]},{"featureType":"all","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"all","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#333739"},{"weight":0.8}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#2ecc71"}]},{"featureType":"landscape.natural","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"all","stylers":[{"color":"#2ecc71"},{"lightness":-7}]},{"featureType":"poi.park","elementType":"all","stylers":[{"color":"#2ecc71"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#333739"},{"weight":0.3},{"lightness":10}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#2ecc71"},{"lightness":-28}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#2ecc71"},{"visibility":"on"},{"lightness":-15}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#2ecc71"},{"lightness":-18}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#2ecc71"},{"lightness":-34}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#333739"}]}],
     center: new google.maps.LatLng(-25.2744,133.7751),
     mapTypeId: 'terrain'
   });
